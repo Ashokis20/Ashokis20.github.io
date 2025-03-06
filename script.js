4f583ab57efe0fbb5b0a33348814d449062f13a8
Ashokis20 <67882421+Ashokis20@users.noreply.github.com>
2025-03-06 21:05:30 +0530

    script.js

--- script.js
+++ script.js
@@ -1,71 +1,11 @@
 window.onload = function() {
     let msg = new SpeechSynthesisUtterance();
-    msg.text = "ABout Me let's start...  SOLO ENTHUSIASTIC.
-Hi, My name is ASHOK DHARMAVARAM currently a fresher but i will be a game developer, Since childhood, im so fascinating about games, from nes to AAA titles i played and enjoyed, my doubts about how developers make this heavens are raised so often and then while in my b.tech final year, i started learning game engines like UE and UNITY, and this is my life.After learning about UE5, I created a realistic desert landscape featuring sandstorms. I added a MetaHuman character to it, showing footprints and incorporating sounds of footsteps. This is just a glimpse of my work, and I'll soon be adding it to the UE marketplace. I hope you enjoy it and education was B.TECH
-I studied ELECTRONIC COMMUNICATION ENGINEERING (ECE) in AUDISANKARA COLLEGE OF ENGINEERING AND TECHNOLOGY GUDUR, in between the years 2017-2022.
-
-Learn more
-INTERMEDIATE
-I studied MPC in NRI JUNIOR COLLEGE, NELLORE, achieving 73.5% in between the years 2015-2017.
-
-Learn more
-CLASS X
-I studied 10th class in PMC HIGH SCHOOL NELLORE, achieving 87% in the year 2015.
-
-Learn more
-    SKILLS
-These are the skills i learned and mastered during my journey.
-
-UNREAL ENGINE AND UNITY3D EDITOR
-I learned and mastered the unreal engine 4 and 5 for 3 years and unity for 1 year.
-
-UNREAL ENGINEUNITY EDITORUNITY EDITOR
-INDUSTRY STANDARD TOOLS LIKE ZBRUSH,MAYA,HOUDINI,3DS MAX,CINEMA 4D,MARMOSET TOOLBAG,DAZ3D,MOTION BUILDER,MARVELOUS DESIGNER,SUBSTANCE 3D PAINTER,SUBSTANCE 3D DESIGNER AND EVEN COMFORTABLE BLENDER AND 20 TOOLS IN ADOBE CREATIVE CLOUD, MOSTLY PHOTOSHOP,ILLUSTRATOR.
-I learned and somehow mastered this all 3D tools one by one after graduation for gaming industry career.
-
-ZBRUSH
-AUTODESK MAYA
-HOUDINI
-AUTODESK 3DS MAX
-CINEMA 4D
-ADOBE SUBSTANCE 3D PAINTER
-ADOBE SUBSTANCE 3D DESIGNER
-MARVELOUS DESIGNER
-ADOBE PHOTOSHOP
-AUTODESK MOTIONBUILDER
-DAZ3D
-MARMOSET TOOLBAG
-ADOBE ILLUSTRATOR
-ADOBE CREATIVE CLOUD
-BLENDER
-C, C#, AND ADVANCED IN C++ FOR VISUAL BLUEPRINT SCRIPTING AND INTERMEDIATE LEVEL OF WEB DEVELOPMENT LANGUAGES, WHICH LIKE FRONTEND, BACKEND, DATABASE AND ADDONS LIKE TYPESCRIPT, WEBBUILDS FROM UNREAL AND UNITY FOR WEBPAGE GAMES, WHICH LEADS TO DEVELOP THIS PORTFOLIO
-I learned this programming languages C++ at intermediate level which is quite advanced for me, even though I learned it but I always prefer Blueprints visual scripting in unreal engine 5 except at the debugging process, that's why I learned C++ for it.
-
-C LANGUAGEC# LANGUAGEC++ LANGUAGEHTML5CSS3JAVASCRIPTWEBGLNODEJSPYTHONMYSQLREDISTYPESCRIPT
-Learn more
-    and this are projects and finally CONTACT ME
-Fill up the form below to send us a message.
-
-Full Name
-Your Name
-Email Address
-you@company.com
-Phone Number
-phone no.
-Your Message
-Your Message
-Send Message
-Address
-PARAMESHWARI NAGAR, NELLORE, ANDHRA PRADESH, INDIA
-Email
-DharmavaramAshok414@gmail.com
-Phone
-+917093598161 and
-Socials";
-    msg.lang = "en-US";  
-    msg.volume = 1;
-    msg.rate = 1;
-    msg.pitch = 1;
+    msg.text = "Welcome to our website! How can we assist you today?";
+    msg.lang = "en-US";  // Change the language if needed
+    msg.volume = 1; // Volume (0 to 1)
+    msg.rate = 1; // Speed (0.1 to 10)
+    msg.pitch = 1; // Pitch (0 to 2)
     
     speechSynthesis.speak(msg);
 };
+
